c9ae046a1d34837780b4668044e8b1e5ca0e4ab6
Robert Williams <paandamonk@gmail.com>
2018-10-01 18:08:56 -0400

    New stuff

--- src/Staff.java
+++ src/Staff.java
@@ -7,26 +7,126 @@ public class Staff extends Person{
 	private String DOB;
 	private Staff Supervisor;
 	private String staffNum;
+	private double salary;
 	private ArrayList<Property> propList = new ArrayList<>(100);
+	private ArrayList<Staff> group = new ArrayList<>(10);
 
-	public Staff(String fname, String lname, int position, String gender, String dOB, String staffNum) {
+	public Staff(String fname, String lname, int position, String gender, String dOB, String staffNum, double salary) {
 		super(fname, lname);
 		this.position = position;
 		this.gender = gender;
 		DOB = dOB;
 		this.staffNum = staffNum;
+		this.salary = salary;
 	}
-	public Staff(String fname, String lname, int position, String gender, String dOB, String staffNum, Staff Supervisor) {
+	public Staff(String fname, String lname, int position, String gender, String dOB, String staffNum, double salary, Staff Supervisor) {
 		super(fname, lname);
 		this.position = position;
 		this.gender = gender;
 		DOB = dOB;
 		this.staffNum = staffNum;
+		this.salary = salary;
 		this.Supervisor = Supervisor;
 	}
 	
 	public void inputData(Property p) {
 		propList.add(p);
+	}
+	/**
+	 * @return the position
+	 */
+	public int getPosition() {
+		return position;
+	}
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(int position) {
+		this.position = position;
+	}
+	/**
+	 * @return the gender
+	 */
+	public String getGender() {
+		return gender;
+	}
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	/**
+	 * @return the dOB
+	 */
+	public String getDOB() {
+		return DOB;
+	}
+	/**
+	 * @param dOB the dOB to set
+	 */
+	public void setDOB(String dOB) {
+		DOB = dOB;
+	}
+	/**
+	 * @return the supervisor
+	 */
+	public Staff getSupervisor() {
+		return Supervisor;
+	}
+	/**
+	 * @param supervisor the supervisor to set
+	 */
+	public void setSupervisor(Staff supervisor) {
+		Supervisor = supervisor;
+	}
+	/**
+	 * @return the staffNum
+	 */
+	public String getStaffNum() {
+		return staffNum;
+	}
+	/**
+	 * @param staffNum the staffNum to set
+	 */
+	public void setStaffNum(String staffNum) {
+		this.staffNum = staffNum;
+	}
+	/**
+	 * @return the salary
+	 */
+	public double getSalary() {
+		return salary;
+	}
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	/**
+	 * @return the propList
+	 */
+	public ArrayList<Property> getPropList() {
+		return propList;
+	}
+	/**
+	 * @param propList the propList to set
+	 */
+	public void setPropList(ArrayList<Property> propList) {
+		this.propList = propList;
+	}
+	/**
+	 * @return the group
+	 */
+	public ArrayList<Staff> getGroup() {
+		return group;
+	}
+	/**
+	 * @param group the group to set
+	 */
+	public void setGroup(ArrayList<Staff> group) {
+		this.group = group;
 	}
 
 //
--- src/Testclass.java
+++ src/Testclass.java
@@ -1,13 +1,81 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class Testclass {
 
 	public static void main(String[] args) {
-		Scanner key = new Scanner(System.in);
+		Connection conn = null;
+		String myDriver = "com.mysql.cj.jdbc.Driver";
+		String myUrl = "jdbc:mysql://localhost:3306/myserver";
+		String username;
+		String password;
+		String query = " insert into users (first_name, last_name, date_created, is_admin, num_points)"
+				+ " values (?, ?, ?, ?, ?)";
 		
+		//All queries for sending information to the MySQL server tables.
+		String rentquery = " insert into renters (renter_number, first_name, last_name, phone_number, staff_first_name,"
+				+ "staff_last_name, prop_street, prop_city, prop_postcode, prop_type, prop_number, number_rooms, monthly_rent,"
+				+ "prop_owner_first_name, prop_owner_last_name)"
+				+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String propertyquery = " insert into properties (prop_street, prop_city, prop_postcode, prop_type, prop_number, number_rooms, monthly_rent,"
+				+ "prop_owner_first_name, prop_owner_last_name)" + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String POquery = " insert into propertyowners (prop_owner_number, prop_owner_first_name, prop_owner_last_name,"
+				+ "prop_street, prop_city, prop_postcode, prop_type, prop_number, number_rooms, monthly_rent,"
+				+ "staff_first_name, staff_last_name)" + " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String BOquery = " insert into business owners (prop_owner_number, prop_owner_first_name, prop_owner_last_name,"
+				+ "prop_street, prop_city, prop_postcode, prop_type, prop_number, number_rooms, monthly_rent,"
+				+ "staff_first_name, staff_last_name, business_name, business_type)" + " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String leasequery = " insert into leases (lease_number, client_first_name, client_last_name, "
+				+ "prop_street, prop_city, prop_postcode, prop_type, prop_number, number_rooms, monthly_rent,"
+				+ "prop_owner_first_name, prop_owner_last_name, paymethod, deposit, deposit_paid,"
+				+ "rent_start, rent_end, duration)" + " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String viewingquery = " insert into viewings (renter_number, first_name, last_name,"
+				+ "prop_street, prop_city, prop_postcode, prop_type, prop_number, number_rooms, monthly_rent,"
+				+ "prop_owner_first_name, prop_owner_last_name, view_date, comments)"
+				+ " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String staffquery = " insert into staff (staff_position, first_name, last_name,"
+				+ "gender, dob, supervisor_first_name, supervisor_last_name)" + " values (?, ?, ?, ?, ?, ?, ?)";
 		
-		
-		key.close();
+		try {
+			Scanner key = new Scanner(System.in);
+			System.out.println("Enter username: ");
+			username = key.nextLine();
+			System.out.println("Enter password: ");
+			password = key.nextLine();
+			Class.forName(myDriver);
+			conn = DriverManager.getConnection(myUrl, username, password);
+
+			if (conn != null) {
+				System.out.println("Connected");
+			}
+
+			// create a sql date object so we can use it in our INSERT statement
+			Calendar calendar = Calendar.getInstance();
+			java.sql.Date startDate = new java.sql.Date(calendar.getTime().getTime());
+
+			// the mysql insert statement
+
+			// create the mysql insert preparedstatement
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			preparedStmt.setString(1, "Barney");
+			preparedStmt.setString(2, "Rubble");
+			preparedStmt.setDate(3, startDate);
+			preparedStmt.setBoolean(4, false);
+			preparedStmt.setInt(5, 5000);
+
+			// execute the preparedstatement
+			preparedStmt.execute();
+
+			conn.close();
+		}
+
+		catch (Exception e) {
+			System.err.println("Got an exception!");
+			System.err.println(e.getMessage());
+		}
 	}
 
 }
