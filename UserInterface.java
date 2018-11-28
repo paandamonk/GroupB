037bcd891512b0a8bd0a3e0b84e4a7e0463eadfd
Unknown <letsdosumgames@gmail.com>
2018-11-28 15:20:11 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 2931c60a88a91a476ba2172dc88625acf809be0e.

--- UserInterface.java
+++ UserInterface.java
@@ -1,5 +1,4 @@
 package pp;
-
 import javax.swing.JFrame;
 import javax.swing.*;
 import java.util.*;
@@ -10,23 +9,14 @@ import java.awt.event.ActionListener;
 import java.awt.*;
 
 public class UserInterface extends JFrame implements ActionListener {
+	
+	
 
-	Staff Rob = new Staff("Rob", "Williams", 2);
-	Staff Kenny = new Staff("Kenny", "Manning", 1);
-	Staff Connor = new Staff("Connor","Colabella",0);
-
-	StaffAccount Robacc = new StaffAccount(Rob.getFname(), "robman", Rob);
-	StaffAccount Kennyacc = new StaffAccount(Kenny.getFname(), "kenman", Kenny);
-	StaffAccount Connoracc = new StaffAccount(Connor.getFname(),"cman",Connor);
-
-	private StaffAccount[] staffAcc = { Robacc, Kennyacc, Connoracc};
-
-	private String[] Manager = { "Rob", "robman", "Dave", "daveman" };
-	private String[] Supervisors = { "Kenny", "kenman", "Bob", "bobman" };
-	private String[] Agents = { "Connor", "cman", "Lucas", "lman" };
-	private String[] allStaff = { "Rob", "robman", "Dave", "daveman", "Kenny", "kenman", "Bob", "bobman", "Connor",
-			"cman", "Lucas", "lman" };
-
+	private String[] Manager = {"Rob","robman","Dave","daveman"};
+	private String[] Supervisors = {"Kenny", "kenman","Bob","bobman"};
+	private String[] Agents = {"Connor", "cman","Lucas","lman"};
+	
+	
 	private JPanel usP;
 	private JPasswordField PasswordText;
 	private JTextField UsernameText;
@@ -36,24 +26,23 @@ public class UserInterface extends JFrame implements ActionListener {
 	private JPanel manPanel;
 	private JLabel man;
 	private JFrame second;
-
-	public UserInterface() {
+	
+	public UserInterface(){
 		setTitle("Proper Properties");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setLayout(new GridLayout(3, 2));
-		setSize(1500, 3000);
+		setLayout(new GridLayout(3,2));
+		setSize(1500,3000);
 		buildLogin();
 		add(usP, BorderLayout.WEST);
 		pack();
 		setVisible(true);
 	}
-
-	private void buildLogin() {
+	private void buildLogin(){
 		usP = new JPanel();
 		UserPrompt = new JLabel("Username: ");
-		UserPrompt.setFont(new Font("Times New Roman", 15, 15));
+		UserPrompt.setFont(new Font("Times New Roman", 15,15));
 		UsernameText = new JTextField(20);
-		UsernameText.setEditable(true);
+		UsernameText.setEditable(true);		
 		PassPrompt = new JLabel("Password: ");
 		PasswordText = new JPasswordField(20);
 		PasswordText.setEditable(true);
@@ -65,28 +54,114 @@ public class UserInterface extends JFrame implements ActionListener {
 		usP.add(PassPrompt);
 		usP.add(PasswordText);
 		usP.add(Login);
-
+		/*final String Username = UsernameText.getText();
+		final String Password = PasswordText.getText();
+		if(checkPass(Username, Password)) TopMenu(Username);
+		else{
+			System.out.println("Invalid Password");
+			buildLogin();
+		}*/
+		
 	}
-
-
-	public void actionPerformed(ActionEvent e) {
-		String action = e.getActionCommand();
-
-		String user = UsernameText.getText();
-		String pass = PasswordText.getText();
-		boolean in = false;
-		if (action.equals("Login")) {
-			for (int i = 0; i < staffAcc.length; i++) {
-				if (staffAcc[i].getUser().equals(user) && staffAcc[i].getPass().equals(pass)) {
-					in = true;
-					setVisible(false);
-					setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-					Menu menu = new Menu(staffAcc[i].getStaff().getPosition());
-				}
-			}
-			if(in == false)
-				JOptionPane.showMessageDialog(null, "Invalid username/password. \n Try again.");
-
+	
+	
+	public boolean checkPass(String user, String Password){
+			return true;
+		//if(user.getpass() == Password)	return true;
+		//else return false;
+	}
+	/*private void TopMenu(String user){
+		int CL = checkClear(user);
+		if(CL == 3){
+			System.out.println("Welcome, Rob");
+			ManagerMenu();
+			
 		}
+		if(CL == 2){
+			System.out.println("Welcome, Kenny");
+			SupMenu();
+		}
+		if(CL == 1){
+			System.out.println("welcome, connor");
+			AgentMenu();
+		}*/
+		
+	private int checkClear(String user){
+		int CL = 0;
+		if(user == "rob"){
+			return(3);
+		}
+		if(user == "kenny"){
+			return(2);
+		}
+		if(user == "connor"){
+			return(1);
+		}
+		return 0;
+		//int CL = user.getCL();
+		//return CL;
 	}
+	
+
+	
+	 public void actionPerformed(ActionEvent e){
+	 String action = e.getActionCommand();
+	 
+	 String user = UsernameText.getText();
+	 String pass = PasswordText.getText();
+	 
+	 
+	if(action.equals("Login")){
+		for(int i = 0; i< Manager.length; i+=2){
+		if(Manager[i].equals(user)&& Manager[i+1].equals(pass)){
+			setVisible(false);
+			setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			ManagerMen m = new ManagerMen();
+			
+		}
+		
+				
+		
+		}
+	
+		
+		for(int i = 0; i<Supervisors.length;i+=2){
+			if(Supervisors[i].equals(user)&& Supervisors[i+1].equals(pass)){
+				setVisible(false);
+				setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				SubMenu s = new SubMenu();
+				
+			}
+			
+			
+		}
+		
+		
+		for(int i = 0; i< Agents.length;i+=2){
+			if(Agents[i].equals(user)&& Agents[i+1].equals(pass)){
+				setVisible(false);
+				setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+				AgentMenu a = new AgentMenu();
+				
+			}
+			
+			
+		}
+	
+	
+	
+	 }
+	
+	
+	
+
+	
+	}
+	
 }
+
+
+
+	
+
+
