3fd00348e254e4ac76c1a031e40b22e34525d532
Unknown <letsdosumgames@gmail.com>
2018-11-30 17:33:18 -0500

    Created SQLite Database

---
+++ src/pp/Backend/FileReader.java
@@ -0,0 +1,37 @@
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class FileReader
+{
+    private String line;
+    private String fname;
+    private String lname;
+    private String age;
+    private String sex;
+    private String[] tokens;
+    
+    FileReader(){};
+    
+    
+    File names = new File("C:/Users/theme/Desktop/names.txt");
+        
+    Scanner info = new Scanner(names);
+        
+    line = info.nextLine();
+        
+        
+        
+        while(line != null){
+            tokens = line.split(",");
+            System.out.println("First: " + tokens[0] + "Last" +tokens[1]);
+            
+            line = info.nextLine();
+        }
+        
+        
+    }
+    
+    }
+}
