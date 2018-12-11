bf81d65feb1b069cf922d520d6b88bef7e031bd9
Unknown <letsdosumgames@gmail.com>
2018-12-11 16:17:31 -0500

    Completed name and digit authenticator methods

--- src/pp/Backend/InputAuthenticator.java
+++ src/pp/Backend/InputAuthenticator.java
@@ -6,8 +6,11 @@ public class InputAuthenticator {
         //Examples of use
         InputAuthenticator ia = new InputAuthenticator();
 
+        System.out.println("Name authenticator:");
         System.out.println(ia.nameAuthenticator("rOger"));
-        System.out.println(ia.nameAuthenticator("ronnor"));
+        System.out.println(ia.nameAuthenticator("roger"));
+        System.out.println(ia.nameAuthenticator("Rog3r"));
+        System.out.println(ia.nameAuthenticator("R()ger"));
         System.out.println(ia.nameAuthenticator("Roger"));
         System.out.println(ia.nameAuthenticator("RogeR"));
 
@@ -18,15 +21,45 @@ public class InputAuthenticator {
         else{ // name authenticator returned false
             System.out.println("Name authentication failed.");
         }
+        System.out.println();
+        System.out.println("Digit authenticator:");
+        System.out.println(ia.digitAuthenticator("123FOUR5"));
+        System.out.println(ia.digitAuthenticator("aab456"));
+        System.out.println(ia.digitAuthenticator("12345"));
+        System.out.println(ia.digitAuthenticator("!@#$5^&*("));
+
+        String digitInput = "12345";
+        if(ia.digitAuthenticator(digitInput)) { // name authenticator returned true
+            int digitsForStorage = Integer.valueOf(digitInput);
+            System.out.println("Digit authentication succeeded. Value being stored is: " + digitsForStorage);
+            System.out.println("Proof that input is now an integer, here's the input - 1: " + (digitsForStorage - 1));
+        }
+        else{ // name authenticator returned false
+            System.out.println("Name authentication failed.");
+        }
     }
 
+    //Returns true if string passed contains only numbers
+    public boolean digitAuthenticator(String digits){
+        for(int i = 0; i < digits.length(); i++){
+            if(!Character.isDigit(digits.charAt(i))){
+                return false;
+            }
+            else if(Character.isDigit(digits.charAt(i)) && i == (digits.length() - 1)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Returns true if string passed contains only letters, with the first letter being uppercase and the remaining lowercase.
     public boolean nameAuthenticator(String name){
         if(Character.isUpperCase(name.charAt(0))){
             for(int i = 1; i < name.length(); i++){
-                if(Character.isUpperCase(name.charAt(i))){
+                if(Character.isUpperCase(name.charAt(i)) || !Character.isLetter(name.charAt(i))){
                     return false;
                 }
-                else if(!Character.isUpperCase(name.charAt(i)) && i == (name.length() - 1)){
+                else if(!Character.isUpperCase(name.charAt(i)) && i == (name.length() - 1) && Character.isLetter(name.charAt(name.length() - 1))){
                     return true;
                 }
             }
